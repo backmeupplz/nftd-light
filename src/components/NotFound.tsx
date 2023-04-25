08cb1611dc04bcddabf5e40ee4d18f223db5a1e7
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-04-25 09:47:47 -0700

    turn on redirect on error

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'preact/hooks'
 
 export default function ({ username }: { username?: string }) {
-  // useEffect(() => {
-  //   window.location.href = username
-  //     ? `https://nf.td/${username}`
-  //     : 'https://nf.td'
-  // })
+  useEffect(() => {
+    window.location.href = username
+      ? `https://nf.td/${username}`
+      : 'https://nf.td'
+  })
   return null
 }
