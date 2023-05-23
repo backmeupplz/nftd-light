5cc67fc9eb98b65cb526233737c605301b3bc1b1
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-05-23 09:28:21 -0700

    add authorization

--- src/helpers/getUserProfile.ts
+++ src/helpers/getUserProfile.ts
@@ -1,7 +1,12 @@
 import UserProfile from 'models/UserProfile'
 
 export default function (username: string) {
-  return fetch(`https://nf.td/api/public/v1/user?name=${username}`)
+  return fetch(`https://nf.td/api/public/v1/user?name=${username}`, {
+    headers: {
+      Authorization:
+        'KUOUYqx7R0KihHHRtx034Gs7xh8QiRhcsP7w26wmPkQ7z9u7vM2hjnRSundqlPqT',
+    },
+  })
     .then(
       (res) =>
         res.json() as Promise<{ data: UserProfile[] } & { error?: string }>
