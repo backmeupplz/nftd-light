114c5e28c3ef476027430d7bd29a4bf83aa5aa03
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-05-30 15:30:04 -0700

    turn off headers for now

--- src/helpers/getUserProfile.ts
+++ src/helpers/getUserProfile.ts
@@ -2,10 +2,10 @@ import UserProfile from 'models/UserProfile'
 
 export default function (username: string) {
   return fetch(`https://nf.td/api/public/v1/user?name=${username}`, {
-    headers: {
-      Authorization:
-        'KUOUYqx7R0KihHHRtx034Gs7xh8QiRhcsP7w26wmPkQ7z9u7vM2hjnRSundqlPqT',
-    },
+    // headers: {
+    //   Authorization:
+    //     'KUOUYqx7R0KihHHRtx034Gs7xh8QiRhcsP7w26wmPkQ7z9u7vM2hjnRSundqlPqT',
+    // },
   })
     .then(
       (res) =>
